7dcf8e0138d176180150dc5249c04edee7303dc0
yigitserin <yigit.serin@cemametrix.com.tr>
2019-01-15 16:16:59 +0300

    Added triangle view

---
+++ SwiftComboBoxTriangleView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftComboBoxTriangleView.swift
+//  Pods-SwiftComboBox_Example
+//
+//  Created by Yigit Serin on 15.01.2019.
+//
+
+import UIKit
+
+class SwiftComboBoxTriangleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
